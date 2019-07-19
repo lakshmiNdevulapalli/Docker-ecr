0102a356d3cc268c42e05aa21cfe84f19b5bfd40
Lakshmi N Bala Devulapalli Venkata <baludevulapalli@Lakshmis-MacBook-Pro.local>
2019-07-19 13:55:58 -0700

    .

--- myapp.tf
+++ myapp.tf
@@ -1,29 +1,33 @@
+# app
+
 data "template_file" "myapp-task-definition-template" {
-  template = "${file("template/app.json.tpl")}"
+  template               = "${file("templates/app.json.tpl")}"
   vars = {
-      REPOSITORY_URL = "${replace("${aws_ecr_repository.myapp.repository_url}", "https://", "")}"
+    REPOSITORY_URL = "${replace("${aws_ecr_repository.myapp.repository_url}", "https://", "")}"
   }
 }
+
 resource "aws_ecs_task_definition" "myapp-task-definition" {
-  family = "service"
+  family                = "myapp"
   container_definitions = "${data.template_file.myapp-task-definition-template.rendered}"
 }
+
 resource "aws_elb" "myapp-elb" {
   name = "myapp-elb"
 
   listener {
-      instance_port = 3000
-      instance_protocol = "http"
-      lb_port = 80
-      lb_protocol = "http"
+    instance_port = 3000
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
   }
 
   health_check {
-      healthy_threshold = 3
-      unhealthy_threshold = 3
-      timeout = 30
-      target = "HTTP:3000/"
-      interval = 60
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    timeout = 30
+    target = "HTTP:3000/"
+    interval = 60
   }
 
   cross_zone_load_balancing = true
@@ -31,13 +35,14 @@ resource "aws_elb" "myapp-elb" {
   connection_draining = true
   connection_draining_timeout = 400
 
-  subnets = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"]
+  subnets = ["${aws_subnet.main-public-1.id}","${aws_subnet.main-public-2.id}"]
   security_groups = ["${aws_security_group.myapp-elb-securitygroup.id}"]
 
-  tags = {
-      Name = "myapp-elb"
+  tags =  {
+    Name = "myapp-elb"
   }
 }
+
 resource "aws_ecs_service" "myapp-service" {
   name = "myapp"
   cluster = "${aws_ecs_cluster.example-cluster.id}"
@@ -47,12 +52,9 @@ resource "aws_ecs_service" "myapp-service" {
   depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
 
   load_balancer {
-      elb_name = "${aws_elb.myapp-elb.name}"
-      container_name = "myapp"
-      container_port = 3000
+    elb_name = "${aws_elb.myapp-elb.name}"
+    container_name = "myapp"
+    container_port = 3000
   }
-
-  lifecycle { 
-      ignore_changes = ["task_definition"]
-  }
+  lifecycle { ignore_changes = ["task_definition"] }
 }
